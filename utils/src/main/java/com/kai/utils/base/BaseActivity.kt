9a0806a15300a6b6e7b1e88f14b0a6ddb22c0534
HanKaiYang <274129906@qq.com>
2019-12-27 17:10:00 +0800

    Initial commit

---
+++ utils/src/main/java/com/kai/utils/base/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.kai.utils.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * @Description TODO
+ * @Author HanKaiYang
+ * @CreateDate 2019/12/27 16:25
+ * @Version 1.0
+ */
+class BaseActivity : AppCompatActivity() {
+
+}
